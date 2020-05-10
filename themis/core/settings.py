616a474e0ac816195234f077ec3c5960bcaa0d32
Xavier Mathew <xavier@compile.com>
2020-05-10 14:29:09 +0530

    add celery settings

--- themis/core/settings.py
+++ themis/core/settings.py
@@ -175,6 +175,14 @@ logging.getLogger('asyncio').setLevel(logging.WARNING)
 
 
 # celery
+# https://www.cloudamqp.com/docs/celery.html
+CELERY_BROKER_URL = None
+CELERY_BROKER_POOL_LIMIT = 1 # Will decrease connection usage
+CELERY_BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
+CELERY_BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
+CELERY_RESULT_BACKEND = None # AMQP is not recommended as result backend as it creates thousands of queues
+CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
+CELERY_WORKER_PREFETCH_MULTIPLIER = 1 # Disable prefetching, it's causes problems and doesn't help performance
 CELERY_IGNORE_RESULT = True
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
