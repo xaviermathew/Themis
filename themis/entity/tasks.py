c3d5d1e4032e9fb472434f483c22dd0d08f96388
Xavier Mathew <xavier@compile.com>
2020-05-17 04:38:27 +0530

    (fix) using CT instead of model

--- themis/entity/tasks.py
+++ themis/entity/tasks.py
@@ -5,6 +5,6 @@ from themis.core.celery import app as celery_app
 
 @celery_app.task(ignore_result=True)
 def crawl_tweets_async(entity_ct, entity_id, limit):
-    klass = ContentType.objects.get_for_id(entity_ct)
+    klass = ContentType.objects.get_for_id(entity_ct).model_class()
     entity = klass.objects.get(pk=entity_id)
     entity.crawl_tweets(limit)
