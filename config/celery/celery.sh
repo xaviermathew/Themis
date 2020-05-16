8b512268a300a855abe24597dde145dd1cbc2c81
Xavier Mathew <xavier@compile.com>
2020-05-17 04:36:46 +0530

    add Type=forking to celery systemd conf and add conf for individual cmds

--- config/celery/celery.sh
+++ config/celery/celery.sh
@@ -4,22 +4,14 @@ VIRTUALENV_BIN=/home/ubuntu/virtual_env/themis/bin
 cd /home/ubuntu/Themis
 source $VIRTUALENV_BIN/activate && source $VIRTUALENV_BIN/postactivate
 
-CMD=$(cat <<COMMAND_SUFFIX
-    2
-	-A themis.core
-    --pidfile=pids/celery/%N.pid
-    --hostname=celery%i@%h
-	-l INFO
-	--logfile=logs/celery/%N.log
-    --without-gossip --without-mingle --without-heartbeat
-    -Q:1 T_crawl_feed,T_process_article,T_crawl_twitter -c:1 1
+echo "${1-start}ing celery"
+DJANGO_SETTINGS_MODULE=themis.core.settings $VIRTUALENV_BIN/celery multi ${1} \
+    2 \
+	-A themis.core \
+    --pidfile=pids/celery/%N.pid \
+    --hostname=celery%i@%h \
+	-l INFO \
+	--logfile=logs/celery/%N.log \
+    --without-gossip --without-mingle --without-heartbeat \
+    -Q:1 T_crawl_feed,T_process_article,T_crawl_twitter -c:1 1 \
     -Q:2 T_process_tweet -c:2 3
-COMMAND_SUFFIX
-)
-
-echo "stopping celery"
-DJANGO_SETTINGS_MODULE=themis.core.settings $VIRTUALENV_BIN/celery multi stop $CMD
-echo "waiting for 3s"
-sleep 3
-echo "starting celery"
-DJANGO_SETTINGS_MODULE=themis.core.settings $VIRTUALENV_BIN/celery multi restart $CMD
