4e264c8de90577230df5aea6ebe686f39342aef6
Xavier Mathew <xavier@compile.com>
2020-05-29 00:53:19 +0530

    copy code from django static to avoid having to set DEBUG=True

--- themis/core/urls.py
+++ themis/core/urls.py
@@ -1,12 +1,15 @@
+import re
+
 from django.conf import settings
 from django.conf.urls import url
-from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import re_path
+from django.views.static import serve
 
 from themis.core import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + [
+    re_path(r'^%s(?P<path>.*)$' % re.escape(settings.STATIC_URL.lstrip('/')), serve, document_root=settings.STATIC_ROOT),
     url(r'^$', views.home),
 ]
