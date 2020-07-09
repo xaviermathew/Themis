998f4923f35171b0a97cb4591c0f47451e73b895
Xavier Mathew <xavier@compile.com>
2020-07-10 00:29:04 +0530

    (fix) possible bug in twitter crawl

--- mnemonic/news/utils/twitter_utils.py
+++ mnemonic/news/utils/twitter_utils.py
@@ -3,12 +3,11 @@ import twint
 
 def get_tweets_for_username(username, limit=None):
     c = twint.Config()
-
     c.Username = username
     if limit:
         c.Limit = limit
+    else:
+        c.User_full = True
     c.Store_object = True
-    c.User_full = True
-
     twint.run.Search(c)
     return twint.output.tweets_list
