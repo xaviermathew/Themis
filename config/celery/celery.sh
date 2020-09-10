3c10868024d3bc7efc93222d76dd5776a108e521
Xavier Mathew <xavier@compile.com>
2020-09-11 00:13:35 +0530

    reduce celery conc further

--- config/celery/celery.sh
+++ config/celery/celery.sh
@@ -6,12 +6,11 @@ source $VIRTUALENV_BIN/activate && source $VIRTUALENV_BIN/postactivate
 
 echo "${1-start}ing celery"
 DJANGO_SETTINGS_MODULE=mnemonic.core.settings $VIRTUALENV_BIN/celery multi ${1} \
-    2 \
+    1 \
 	-A mnemonic.core \
     --pidfile=pids/celery/%N.pid \
     --hostname=celery%i@%h \
 	-l INFO \
 	--logfile=logs/celery/%N.log \
     --without-gossip --without-mingle --without-heartbeat \
-    -Q:1 T_crawl_feed,T_process_article -c:1 1 \
-    -Q:2 T_crawl_twitter,T_process_tweet -c:2 1
+    -Q:1 T_crawl_feed,T_process_article,T_crawl_twitter,T_process_tweet -c:1 1
